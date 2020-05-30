83c6fb2deeb2a0c20a47ea0d0eb5795d50fb5290
ledovskayayuliya <65966183+ledovskayayuliya@users.noreply.github.com>
2020-05-30 12:58:50 +0300

    Add files via upload
    Training. Defining a point inside or outside the circle area

---
+++ defining a point inside or outside the circle area.py
@@ -0,0 +1,17 @@
+Python 3.8.2 (tags/v3.8.2:7b3ab59, Feb 25 2020, 22:45:29) [MSC v.1916 32 bit (Intel)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+>>> radius = 42
+area_of_circle = (3.1415926 * radius) **2
+
+print(round(area_of_circle, 4))
+
+point_1 = (23, 34)
+distance_point_1 = (point_1[0] **2 + point_1[1] **2) ** 0.5
+
+print((area_of_circle) > distance_point_1)
+
+point_2 = (30, 30)
+distance_point_2 = (point_2[0] **2 + point_2[1] **2) ** 0.5
+
+print((area_of_circle) > distance_point_2)
